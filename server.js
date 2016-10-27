aa3b7bb5779c1327f558503c05c705dc22d65751
Ihor Bondarenko <ihor.bondarenko@acropolium.com>
2016-10-27 18:21:28 +0300

    edited

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ var io = require('socket.io')(4000);
 //io.adapter(redis({ host: 'localhost', port: 6379 }));
 var Redis = require('ioredis');
 var _ = require('lodash');
-
+var moderators = [];
 io.sockets.on('connection', function(socket){
     console.log('connected');
+    moderators.push(socket);
     var redis = new Redis({
         port: 6379});
     redis.subscribe('private-message-created-channel', function (err, count) {
     });
     redis.on('message', function (channel, message) {
-        console.log('Receive message %s from channel %s', message, channel);
-        io.sockets.emit(channel, message);
+       // console.log('Receive message %s from channel %s', message, channel);
+        console.log(moderators.length);
+        var data = JSON.parse(message);
+
+        socket.broadcast.emit(channel, message);
     });
 
     socket.on('join', function(data){
