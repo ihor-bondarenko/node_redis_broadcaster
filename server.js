57727d5fabd9a8150728c69ec7597bf1a36f54fa
Ihor Bondarenko <ihor.bondarenko@acropolium.com>
2016-10-27 18:59:54 +0300

    ed

--- server.js
+++ server.js
@@ -4,18 +4,19 @@ var io = require('socket.io')(4000);
 var Redis = require('ioredis');
 var _ = require('lodash');
 var moderators = [];
+var redis = new Redis({
+    port: 6379});
+redis.subscribe('private-message-created-channel', function (err, count) {
+});
 io.sockets.on('connection', function(socket){
-    console.log('connected');
-    moderators.push(socket);
-    var redis = new Redis({
-        port: 6379});
-    redis.subscribe('private-message-created-channel', function (err, count) {
-    });
-    redis.on('message', function (channel, message) {
-       // console.log('Receive message %s from channel %s', message, channel);
-        console.log(moderators.length);
-        var data = JSON.parse(message);
+    var clients = io.sockets.clients();
+    moderators.push(socket.id);
+    console.log(moderators);
 
+    redis.on('message', function (channel, message) {
+        console.log('Receive message %s from channel %s', message, channel);
+        var data = JSON.parse(message);
+console.log('message');
         socket.broadcast.emit(channel, message);
     });
 
@@ -25,5 +26,8 @@ io.sockets.on('connection', function(socket){
     });
     socket.on('ping', function(data){
         io.sockets.emit('ping', {username: socket.username});
+    });
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
     });
 });
